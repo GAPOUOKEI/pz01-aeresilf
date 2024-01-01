62e22d5cb1599478d6c4aa9f61ee1873702fb1c6
egurnova elena <gamedev409@yandex.ru>
2024-01-01 22:38:18 +0500

    решено

--- consoleProject/Program.cs
+++ consoleProject/Program.cs
@@ -1,15 +1,18 @@
 ﻿namespace consoleProject
 {
     /*
-     * ФИО студента: 
-     * номер варианта: 
-     * условие задачи (скопировать из листа задания): 
+     * ФИО студента: Иванов Иван
+     * номер варианта: 2
+     * условие задачи (скопировать из листа задания): сложить два введенных числа
      */
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int x = Int32.Parse(Console.ReadLine());
+            int y = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(x+y);
         }
     }
 }
